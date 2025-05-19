0a65284e5701b98e739393e63735a801280ff335
pallavitechbird <pallavi.mankar@techbirdit.in>
2025-05-19 12:15:05 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Welsome to NextJS + Chakra UI app + Tailwind CSS</h1>
       <Button onClick={() => router.push("/about")}>About</Button>
       <Button onClick={() => router.push("/contact")}>Contact</Button>
     </div>
